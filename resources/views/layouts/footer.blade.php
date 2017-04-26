c0bab3816c01bd34376dea7a88a4c9e5361cc46b
Matas Kinderis <mataskind@gmail.com>
2017-04-26 08:53:29 +0300

    Small tweak

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="footer">
     <div class="col-md-offset-2">
-        <p style="margin-top: 5px;"><a target="_blank" href="http://kinderis94.com">Matas Kinderis</a> | <a href="https://github.com/kinderis94/baldai_cms">github</a></p>
+        <p style="margin-top: 5px;"><a href="https://github.com/kinderis94/baldai_cms">github</a></p>
     </div>
 </div>
 
@@ -8,7 +8,6 @@
     .footer {
         height: 35px;
         width:100%;
-        background-color: #B1B1AF;
         position:fixed;
         left:0px;
         bottom:0px;
